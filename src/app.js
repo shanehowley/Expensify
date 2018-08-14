8291aa084361c2bb534c6e3658f460c68f41a59c
shanehowley <snhowley@gmail.com>
2018-08-14 10:43:57 +0100

    Method Binding

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+// const obj = {
+//     name: 'vikram',
+//     getName() {
+//         return this.name;
+//     }
+// };
+
+
+
+// console.log(obj.getName());
+
+
 class IndecisionApp extends React.Component {
     render() {
         const title = 'Indecision';
@@ -42,8 +54,14 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
+
     handleRemoveAll() {
-        alert('removeall');
+        console.log(this.props.options);
+        // alert('removeall');
     }
     
     render() {
